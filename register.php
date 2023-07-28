e7ed2de30de5b7709305b4b7a3d772edec2b67f9
Dev-Mood <126387763+Dev-Mood@users.noreply.github.com>
2023-07-28 13:14:22 +0800

    Create register.php

---
+++ register.php
@@ -0,0 +1,17 @@
+<form action="register_process.php" method="POST">
+    <h2>Registration Form</h2>
+    
+    <input name="fullname" type="text" placeholder="Enter fullname">
+    
+    <input name="username" type="text" placeholder="Enter username">
+    
+    <input name="email" type="text" placeholder="Enter email">
+    
+    <input name="contact_number" type="number" placeholder="Enter contact number">
+    
+    <input name="location" type="text" placeholder="Enter location">
+    
+    <input name="password" type="password" placeholder="Enter password">
+    
+    <input name="submit" type="submit" value="Create Account">
+</form>
